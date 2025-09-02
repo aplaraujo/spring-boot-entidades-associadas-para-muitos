5e84d490aac50e32a9b109750ae17890e80b906f
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-09-02 18:30:48 -0300

    first commit

---
+++ src/main/java/com/example/spring_boot_entidades_associadas_para_muitos/SpringBootEntidadesAssociadasParaMuitosApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_entidades_associadas_para_muitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEntidadesAssociadasParaMuitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEntidadesAssociadasParaMuitosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_entidades_associadas_para_muitos/SpringBootEntidadesAssociadasParaMuitosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_entidades_associadas_para_muitos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEntidadesAssociadasParaMuitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
