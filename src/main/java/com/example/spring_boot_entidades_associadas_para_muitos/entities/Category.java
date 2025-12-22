375cbafab92eb6fd5297d7b85d99b70d1df9e585
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-12-22 20:14:40 -0300

    Entities refactored and Lombok included

--- src/main/java/com/example/spring_boot_entidades_associadas_para_muitos/entities/Category.java
+++ src/main/java/com/example/spring_boot_entidades_associadas_para_muitos/entities/Category.java
@@ -9,9 +9,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name="tb_category")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Category {
 
     @Id
@@ -22,30 +30,4 @@ public class Category {
     @ManyToMany(mappedBy="categories")
     private Set<Product> products = new HashSet<>();
 
-    public Category() {}
-
-    public Category(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Set<Product> getProducts() {
-        return products;
-    }
 }
--- src/main/java/com/example/spring_boot_entidades_associadas_para_muitos/entities/Product.java
+++ src/main/java/com/example/spring_boot_entidades_associadas_para_muitos/entities/Product.java
@@ -11,9 +11,17 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name="tb_product")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Product {
 
     @Id
@@ -26,39 +34,5 @@ public class Product {
     @JoinTable(name="tb_product_category", joinColumns=@JoinColumn(name="product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
-    public Product() {}
-
-    public Product(Long id, String name, Double price) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Set<Category> getCategories() {
-        return categories;
-    }
+    
 }
